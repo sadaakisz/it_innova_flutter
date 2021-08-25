5fae9f3daa9755a6e2f232ce093bf3f523a3e2db
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-25 00:44:39 -0500

    Created baseline for History tab.

---
+++ lib/pages/history.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:it_innova_flutter/models/heart_rate_history.dart';
+
+class History extends StatelessWidget {
+  List<HeartRateHistory> = <HeartRateHistory>[
+
+  ];
+  const History({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
