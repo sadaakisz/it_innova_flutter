9f8b8f9633a0f5210b5629dba75d2aef419f671c
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-08-25 10:03:20 -0500

    Added mock values and implemented ListView.

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -2,13 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:it_innova_flutter/models/heart_rate_history.dart';
 
 class History extends StatelessWidget {
-  List<HeartRateHistory> = <HeartRateHistory>[
-
+  History({Key? key}) : super(key: key);
+  final List<HeartRateHistory> _heartRateHistoryList = [
+    HeartRateHistory(bpm: 80, date: '23/04/2021'),
+    HeartRateHistory(bpm: 100, date: '01/05/2021'),
+    HeartRateHistory(bpm: 95, date: '22/05/2021'),
   ];
-  const History({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      itemCount: _heartRateHistoryList.length,
+      itemBuilder: (BuildContext context, int index) {
+        var heartRateItem = _heartRateHistoryList[index];
+        return ListTile(
+          title: Text(heartRateItem.bpm.toString() + ' BPM'),
+          subtitle: Text(heartRateItem.date),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(height: 5);
+      },
+    );
   }
 }
