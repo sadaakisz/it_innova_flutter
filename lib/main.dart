f27dec163dcb18ae893a019c4f898d0877d5891d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-09-26 12:32:33 -0500

    Initialized SharedPrefs variables.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:it_innova_flutter/pages/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initializeSharedPrefsVariables();
   runApp(MyApp());
+}
+
+_initializeSharedPrefsVariables() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (!prefs.containsKey('bpm')) await prefs.setInt('bpm', 0);
+  if (!prefs.containsKey('bpmTime')) await prefs.setString('bpmTime', '');
+  if (!prefs.containsKey('enabledLocation'))
+    await prefs.setBool('enabledLocation', false);
 }
 
 class MyApp extends StatelessWidget {
