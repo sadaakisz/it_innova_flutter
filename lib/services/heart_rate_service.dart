a36f2fc6f35ff1b8f427fca8e4811d85982c1171
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-10-13 20:26:54 -0500

    Made so the createData function returns a response.

--- lib/services/heart_rate_service.dart
+++ lib/services/heart_rate_service.dart
@@ -5,19 +5,16 @@ import 'package:it_innova_flutter/models/heart_rate_history.dart';
 
 class HeartRateService {
   late HeartRateHistory heartRateHistory;
-  Future<void> createData() async {
+  Future<Response> createData() async {
     final body = heartRateHistory.toJson();
-    try {
-      Response response = await post(
-        Uri.parse('http://40.76.250.197:8080/ritmo/mobile/12/RitmoCardiaco'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(body),
-      );
-      print(response.statusCode);
-    } catch (e) {
-      print('Caught error $e');
-    }
+    Response response = await post(
+      Uri.parse('http://40.76.250.197:8080/ritmo/mobile/12/RitmoCardiaco'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+    print(response.statusCode);
+    return response;
   }
 }
