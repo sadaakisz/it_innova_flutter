b994987d2be0fabf48642562a020ec135444b923
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-10-13 20:27:02 -0500

    Made so the createData function returns a response.

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -5,19 +5,16 @@ import 'package:it_innova_flutter/models/location_value.dart';
 
 class LocationService {
   late LocationValue locationValue;
-  Future<void> createData() async {
+  Future<Response> createData() async {
     final body = locationValue.toJson();
-    try {
-      Response response = await post(
-        Uri.parse('http://40.76.250.197:8080/ubicacion/mobile/12/ubicacion'),
-        headers: <String, String>{
-          'Content-Type': 'application/json',
-        },
-        body: jsonEncode(body),
-      );
-      print(response.statusCode);
-    } catch (e) {
-      print('Caught error $e');
-    }
+    Response response = await post(
+      Uri.parse('http://40.76.250.197:8080/ubicacion/mobile/12/ubicacion'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode(body),
+    );
+    print(response.statusCode);
+    return response;
   }
 }
