163228e855c929bccd3e3c0c944cea0a27043e58
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-10-13 20:25:51 -0500

    Implemented sending heartRateHistory to API.

--- lib/pages/heart_rate.dart
+++ lib/pages/heart_rate.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:it_innova_flutter/models/heart_rate_history.dart';
 import 'package:it_innova_flutter/pages/select_device.dart';
+import 'package:it_innova_flutter/services/heart_rate_service.dart';
+import 'package:it_innova_flutter/util/json_time.dart';
 import 'package:it_innova_flutter/widgets/heart_rate_indicator.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -17,12 +19,23 @@ class _HeartRateState extends State<HeartRate> {
   Timer? timer;
   int bpm = 0;
 
+  HeartRateService service = new HeartRateService();
+
+  void createData() async {
+    String now = JsonTime().getJsonTime();
+    HeartRateHistory heartRateHistory =
+        new HeartRateHistory(bpm: bpm, date: now);
+    service.heartRateHistory = heartRateHistory;
+    await service.createData();
+  }
+
   _getBpm() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     if (bpm != prefs.getInt('bpm')) {
       setState(() {
         bpm = prefs.getInt('bpm')!;
       });
+      createData();
     }
   }
 
