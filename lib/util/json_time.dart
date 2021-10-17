e91b08978b6f073152528dc215eba83094e1a69d
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-10-17 11:17:15 -0500

    Implemented functions to handle Json time strings to legible date and time strings.

--- lib/util/json_time.dart
+++ lib/util/json_time.dart
@@ -10,4 +10,14 @@ class JsonTime {
     DateFormat dateFormat = DateFormat("yyyy-MM-dd");
     return dateFormat.format(DateTime.now());
   }
+
+  fromJsonToStringDate(String json) {
+    return json.replaceAll('-', '/').substring(0, 10);
+  }
+
+  fromJsonToStringTime(String json) {
+    String date = fromJsonToStringDate(json);
+    String time = json.substring(11, 19);
+    return date + ' ' + time;
+  }
 }
