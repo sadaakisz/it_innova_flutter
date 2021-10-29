f6f221a2eec4d01a4ce51fcca22ddaccdffe7295
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-10-28 20:25:30 -0500

    PatientId is retrieved from SharedPrefs to make the correct request.

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:it_innova_flutter/models/location_value.dart';
+import 'package:it_innova_flutter/util/patient_id.dart';
 
 class LocationService {
   late LocationValue locationValue;
   Future<Response> createData() async {
+    final patientId = await PatientId().getId();
     final body = locationValue.toJson();
     Response response = await post(
-      Uri.parse('http://40.76.250.197:8080/ubicacion/mobile/12/ubicacion'),
+      Uri.parse(
+          'http://40.76.250.197:8080/ubicacion/mobile/$patientId/ubicacion'),
       headers: <String, String>{
         'Content-Type': 'application/json',
       },
