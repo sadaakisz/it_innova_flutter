f9a8d56f2ef8aac28a0570ac90a0908771ad4f53
Hector Suzuki <hssm2000.sadaaki@gmail.com>
2021-11-05 21:45:19 -0500

    Modified AlertHistory to match json response from backend.

--- lib/models/alert_history.dart
+++ lib/models/alert_history.dart
@@ -1,6 +1,28 @@
 class AlertHistory {
-  String name;
-  String date;
+  AlertHistory({
+    required this.id,
+    required this.heartRate,
+    required this.name,
+    required this.date,
+  });
+  late int id;
+  late int heartRate;
+  late String name;
+  late String date;
 
-  AlertHistory({required this.name, required this.date});
+  AlertHistory.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    heartRate = json['heartRate'];
+    name = json['transtorno'];
+    date = json['fecha_ritmo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['id'] = id;
+    _data['heartRate'] = heartRate;
+    _data['transtorno'] = name;
+    _data['fecha_ritmo'] = date;
+    return _data;
+  }
 }
